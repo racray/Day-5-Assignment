da2b4143084f919fbac8021a615c2aa04af5a871
Rayed Khan <60972839+racray@users.noreply.github.com>
2021-09-22 19:27:37 +0530

    Update Question 1G.js

--- Question 1G.js
+++ Question 1G.js
@@ -1,6 +1,14 @@
 (function(){
-    var a = [5,2,1,3,5,6,7,8,9,4,4];
-    let unique = [...new Set(a)];
-    
-    console.log(unique);
+var a = [5,2,1,3,5,6,7,8,9,4,4];
+for(let i=0;i<a.length;i++){
+    for(let j=i+1;j<a.length;j++){
+        if(a[i]==a[j]){
+            for(k=j;k<a.length-1;k++){
+                a[k] = a[k+1];
+            }
+            a.length--;
+        }
+    }
+}
+console.log(a);
 })();
